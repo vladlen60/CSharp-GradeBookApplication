d715335eb2a4b8182f93c0274dc70fded659fcf9
VladK <Vladk@iqmetrix.com>
2019-12-02 15:03:32 -0800

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -53,7 +53,7 @@ namespace GradeBook.UserInterfaces
             }
             else
             {
-                Console.WriteLine("{0}  is not a supported type of gradebook, please try again.", type);
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again.", type);
                 return;
             }
             //Console.WriteLine("Created gradebook {0}.", name);
