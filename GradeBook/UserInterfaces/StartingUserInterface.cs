5b7c2551507b6b731d16f1881762929e0ee2450b
VladK <Vladk@iqmetrix.com>
2019-12-02 15:02:07 -0800

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -57,7 +57,7 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             //Console.WriteLine("Created gradebook {0}.", name);
-            //GradeBookUserInterface.CommandLoop(gradeBook);
+            GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
         public static void LoadCommand(string command)
